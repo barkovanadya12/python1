93847bb35977637442742d19e01572755a39e382
barkovanadya12 <barkovanadya12@gmail.com>
2024-06-09 20:43:26 +0500

    commit

--- main2.py
+++ main2.py
@@ -1,7 +1,14 @@
 num1 = int(input ('Введите первое число: '))
 
 num2 = int(input ('Введите второе число: '))
+
+# num1 = num1 + 5
+num1 +=5
+
+
 print('Result:', num1+num2)
 print('Result:', num1-num2)
 print('Result:', num1/num2)
 print('Result:', num1*num2)
+print('Result:', num1**num2)
+print('Result:', num1//num2)
