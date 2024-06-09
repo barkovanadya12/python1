1a5a4a9c7d8915a564b8f00221b4ede10cc7867a
barkovanadya12 <barkovanadya12@gmail.com>
2024-06-09 20:42:58 +0500

    commit

--- main1.py
+++ main1.py
@@ -1,1 +1,21 @@
-first_num = input()
+print("Введите первое число: ")
+first_num = int(input())
+# int - для целых чисел, если нас интересуют дроби (через точку!), то вводим float
+print("Введите второе число: ")
+second_num = int(input())
+
+sum = first_num+second_num
+print(f"Сумма={sum}")
+# print("Сумма = , sum") можно еще так записывать то же самое, но через запятую
+
+#print("Результат:", 6, 7, sep="", end="!\n")
+
+
+# list = [1,3,4,5,6,76,5]
+# hasZero = False
+
+# for item in list:
+#     if item == 0:
+#         hasZero = True 
+#         break
+# # boulen - переменные (правда/ложь)
---
+++ main2.py
@@ -0,0 +1,7 @@
+num1 = int(input ('Введите первое число: '))
+
+num2 = int(input ('Введите второе число: '))
+print('Result:', num1+num2)
+print('Result:', num1-num2)
+print('Result:', num1/num2)
+print('Result:', num1*num2)
